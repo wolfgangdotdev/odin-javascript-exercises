1c95de01957a7c8c57d5d33e823b89b89d3a0813
Sean Vandenberg <svanden96@outlook.com>
2023-11-06 19:51:26 -0600

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+	if (min > max) {
+		let temp = min;
+		min = max;
+		max = temp;
+	}
 
+	if (min < 0 || max < 0) return "ERROR";
+
+	if (typeof min != "number" || typeof max != "number") return "ERROR";
+
+	let total = 0;
+	for (let i = min; i <= max; i++) {
+		total += i;
+	}
+	return total;
 };
 
 // Do not edit below this line
