21ba086df63c8189b36d687b7e41e7f78bd009f0
Sean Vandenberg <svanden96@outlook.com>
2023-11-06 19:38:59 -0600

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+	return array.filter((val) => !args.includes(val));
 };
 
 // Do not edit below this line
