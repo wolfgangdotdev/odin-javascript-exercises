21881b93567a05f5eb39549cfedbd19d02d05358
Sean Vandenberg <svanden96@outlook.com>
2023-11-06 19:10:02 -0600

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, count) {
+	if (count < 0) {
+		return "ERROR";
+	}
 
+	let stringToReturn = "";
+	for (let i = 0; i < count; i++) {
+		stringToReturn += string;
+	}
+	return stringToReturn;
 };
 
 // Do not edit below this line
