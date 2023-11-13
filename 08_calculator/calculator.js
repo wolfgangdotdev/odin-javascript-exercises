f84dfb5eb9b89a3636962c3f2857595adba02f29
Sean Vandenberg <svanden96@outlook.com>
2023-11-12 19:56:36 -0600

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,37 @@
-const add = function() {
-	
+const add = function (numA, numB) {
+	return numA + numB;
 };
 
-const subtract = function() {
-	
+const subtract = function (numA, numB) {
+	return numA - numB;
 };
 
-const sum = function() {
-	
+const sum = function (numArray) {
+	return numArray.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numArray) {
+	return numArray.reduce((sum, current) => sum * current);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+	if (num < 0) return -1;
+	else if (num == 0) return 1;
+	else {
+		return num * factorial(num - 1);
+	}
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
