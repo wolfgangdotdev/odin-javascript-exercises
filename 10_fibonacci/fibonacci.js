ff785929be784fecfb147497a30a8fe9376539b1
Sean Vandenberg <svanden96@outlook.com>
2023-11-12 20:22:18 -0600

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (count) {
+	if (count < 0) return "OOPS";
+	if (count === 0) return 0;
 
+	let firstPrevious = 1;
+	let secondPrevious = 0;
+
+	for (let i = 1; i < count; i++) {
+		let temp = firstPrevious + secondPrevious;
+		secondPrevious = firstPrevious;
+		firstPrevious = temp;
+	}
+
+	return firstPrevious;
 };
 
 // Do not edit below this line
