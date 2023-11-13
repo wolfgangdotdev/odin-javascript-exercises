1e788c1cd485910fc6123989370f2410a88579b7
Sean Vandenberg <svanden96@outlook.com>
2023-11-12 20:41:38 -0600

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+	return people.reduce((oldest, currentPerson) => {
+		const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+		const currentPersonAge = getAge(
+			currentPerson.yearOfBirth,
+			currentPerson.yearOfDeath
+		);
 
+		return oldestAge > currentPersonAge ? oldest : currentPerson;
+	});
 };
+
+function getAge(yearOfBirth, yearOfDeath) {
+	if (!yearOfDeath) {
+		yearOfDeath = new Date().getFullYear();
+	}
+	return yearOfDeath - yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
