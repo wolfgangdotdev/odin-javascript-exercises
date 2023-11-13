87ceb7839b414c2130308624de7b23f2858d8f26
Sean Vandenberg <svanden96@outlook.com>
2023-11-12 20:13:44 -0600

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (string) {
+	let stringToTest = string.toLowerCase().replace(/[\.,?!\s]/g, "");
 
+	return stringToTest.split("").reverse().join("") == stringToTest;
 };
 
 // Do not edit below this line
